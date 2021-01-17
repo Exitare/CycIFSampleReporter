dcb83705a0a15600285b728bffe8210ebbfbe6a2
Exitare <business.exitare@icloud.com>
2021-01-17 12:05:53 -0800

    Updated gitignore, requirements and sources.

--- src/main.py
+++ src/main.py
@@ -24,16 +24,17 @@ st.title("CycIF Sample Reader")
 uploaded_file = st.sidebar.file_uploader("Please select your CycIF data set")
 
 aand_file = None
+# Load the file
 if uploaded_file is not None:
     aand_file = sc.read_h5ad(uploaded_file)
 
+# Only if a file is loaded show all the interactive data
 if aand_file is not None:
 
     df = aand_file.to_df()
-
     st.subheader("Raw data set")
 
-    if aand_file is not None:
+    if st.checkbox('Show raw data set'):
         st.write(df)
 
     violin_plot_cols(df)
